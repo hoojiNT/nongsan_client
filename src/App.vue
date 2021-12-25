f908b9cb535c75bd32d454f48c4f5eafe4b9fd2d
hooji <thehoinguyen@outlook.com>
2021-12-26 00:55:54 +0700

    add template

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <component :is="resolveLayout">
+    <router-view></router-view>
+    <upgrade-to-pro></upgrade-to-pro>
+  </component>
 </template>
 
 <script>
+import { computed } from '@vue/composition-api'
+import { useRouter } from '@/utils'
+import LayoutBlank from '@/layouts/Blank.vue'
+import LayoutContent from '@/layouts/Content.vue'
+import UpgradeToPro from './components/UpgradeToPro.vue'
+
 export default {
-  name: 'App'
+  components: {
+    LayoutBlank,
+    LayoutContent,
+    UpgradeToPro,
+  },
+  setup() {
+    const { route } = useRouter()
+
+    const resolveLayout = computed(() => {
+      // Handles initial route
+      if (route.value.name === null) return null
+
+      if (route.value.meta.layout === 'blank') return 'layout-blank'
+
+      return 'layout-content'
+    })
+
+    return {
+      resolveLayout,
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
